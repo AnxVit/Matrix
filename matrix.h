fb8a87650e4b082ba1a6527a21d4fa41923661fb
Vitaliy <vvtsyvunin@miem.hse.ru>
2022-12-02 23:41:22 +0300

    Create parameters and some methods

--- matrix.h
+++ matrix.h
@@ -1,1 +1,14 @@
 #pragma once
+
+namespace linalg {
+	class Matrix {
+		size_t rows() const noexcept { return m_rows; }
+		size_t culumns() const noexcept { return m_columns; }
+		bool empty() const noexcept { return m_rows == 0; }
+		void reshape(size_t rows, size_t columns);
+	private:
+		double* m_ptr = nullptr;
+		size_t m_rows;
+		size_t m_columns;
+	};
+}
--- matrix.hpp
+++ matrix.hpp
@@ -1,1 +1,11 @@
 #pragma once
+
+void linalg::Matrix::reshape(size_t rows, size_t columns) {
+	if (rows * columns == m_rows * m_columns) {
+		m_rows = rows;
+		m_columns = columns;
+	}
+	else {
+		throw - 1;
+	}
+}
