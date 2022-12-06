841eaa842b3fcbb9d7be8fd4d4a397bd454ca762
Vitaliy <vvtsyvunin@miem.hse.ru>
2022-12-06 13:31:20 +0300

    Create methods for work with memory

--- matrix.h
+++ matrix.h
@@ -4,11 +4,16 @@ namespace linalg {
 	class Matrix {
 		size_t rows() const noexcept { return m_rows; }
 		size_t culumns() const noexcept { return m_columns; }
+		size_t capacity() const noexcept { return m_capacity; }
 		bool empty() const noexcept { return m_rows == 0; }
 		void reshape(size_t rows, size_t columns);
+		void reserve(size_t n);
+		void clear() noexcept { m_rows = 0; m_columns = 0; }
 	private:
 		double* m_ptr = nullptr;
 		size_t m_rows;
 		size_t m_columns;
+		size_t m_capacity;
 	};
 }
+#include "matrix.hpp"
--- matrix.hpp
+++ matrix.hpp
@@ -9,3 +9,26 @@ void linalg::Matrix::reshape(size_t rows, size_t columns) {
 		throw - 1;
 	}
 }
+
+void linalg::Matrix::reserve(size_t capacity) {
+	if (m_capacity >= capacity) return;
+
+	double* tmp_ptr = new double[capacity];
+	for (size_t i = 0; i < m_rows * m_columns; ++i) {
+		tmp_ptr[i] = m_ptr[i];
+	}
+	delete[] m_ptr;
+	m_ptr = tmp_ptr;
+	m_capacity = capacity;
+}
+
+void linalg::Matrix::shrink_to_fit() {
+	if (m_capacity == m_rows * m_columns) return;
+
+	double* tmp_ptr = new double[m_rows * m_columns];
+	for (size_t i = 0; i < m_rows * m_columns; ++i) {
+		tmp_ptr[i] = m_ptr[i];
+	}
+	m_ptr = tmp_ptr;
+	m_capacity = m_size;
+}
